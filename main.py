7916c27891ee4d96496de8f97cfce201650a2ad4
modernpacifist <modernpacifist@gmail.com>
2023-12-12 22:55:01 +0300

    proelium-exercitum:12/12/2023-22:55:01 automated backup commit

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import fastapi
-
+from fastapi import FastAPI, status, Response
 from models import InputModel
 
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
 
 @app.get("/")
-async def read_root(input: InputModel):
-    return {"hello": "world"}
+async def read_root():
+    return {"Status": "Healthy"}
 
 
-if __name__ == "__main__":
-    app.run()
+@app.post("/predict", status_code=201)
+async def predict(input: InputModel, response: Response):
+    data = input.dict()
+    
+    if "" in data.values():
+        response.status_code = status.HTTP_400_BAD_REQUEST
+
+    return {"Prediction": "0"}
--- models.py
+++ models.py
@@ -2,21 +2,19 @@ from pydantic import BaseModel
 
 
 class InputModel(BaseModel):
-    number: int
-    string: str
-    BirthDate:
-    education:
-    employment status:
-    Value:
-    JobStartDate:
-    Position:
-    MonthProfit:
-    MonthExpense:
-    Gender:
-    Family status:
-    ChildCount:
-    SNILS:
-    Merch_code:
-    Loan_amount:
-    Loan_term:
-    Goods_category:
+    BirthDate: str
+    Education: str
+    EmploymentStatus: str
+    Value: int
+    JobStartDate: int
+    Position: str
+    MonthProfit: str
+    MonthExpense: str
+    Gender: str
+    FamilyStatus: str
+    ChildCount: str
+    SNILS: str
+    Merch_code: str
+    Loan_amount: str
+    Loan_term: str
+    Goods_category: str
