022c1b3f6d38d81ccea9db45182bd5664fd4f4fa
modernpacifist <modernpacifist@gmail.com>
2023-12-13 14:55:01 +0300

    proelium-exercitum:13/12/2023-14:55:01 automated backup commit

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI, status, Response
 from pydantic import ValidationError
-from models import InputModel
+
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class InputModel(BaseModel):
+    BirthDate: str
+    Education: str
+    EmploymentStatus: str
+    Value: int
+    JobStartDate: int
+    Position: str
+    MonthProfit: str
+    MonthExpense: str
+    Gender: str
+    FamilyStatus: str
+    ChildCount: str
+    SNILS: str
+    Merch_code: str
+    Loan_amount: str
+    Loan_term: str
+    Goods_category: str
 
 
 @app.get("/")
